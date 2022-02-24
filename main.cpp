2d62fbe262fb2ac8b9de93d293f1450fdcc3edf5
anaximeno <anaximenobrito@gmail.com>
2022-02-24 03:07:52 +0000

    Made some updates

--- main.cpp
+++ main.cpp
@@ -1,47 +1,81 @@
 #include <iostream>
+#include <fstream>
+#include <cassert>
 #include <queue>
+
+#ifndef __has_include
+  static_assert(false, "__has_include not supported");
+#else
+#if __cplusplus >= 201703L && __has_include(<filesystem>)
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#elif __has_include(<boost/filesystem.hpp>)
+    #include <boost/filesystem.hpp>
+    namespace fs = boost::filesystem;
+#endif
+#endif
+
 #include <hashlibpp.h>
+
 using namespace std;
 
 
-class AbstractFile {
-    protected:
-        string path;
-        bool isValidFile;
+class File {
     public:
-        AbstractFile(string path) {
-            this->path = path;
-        }
-        bool isValid() {
-            return this->isValidFile;
-        }
-        string getPath() {
-            return this->path;
-        }
-};
-
-
-class InvalidFile: public AbstractFile {
-    string reason;
-    public:
-        InvalidFile(string path, string reason): AbstractFile(path) {
-            this->reason = reason;
-        }
-};
-
-
-class ValidFile: public AbstractFile {
-    public:
-        ValidFile(string path): AbstractFile(path) {
-            isValidFile = true;
-        }
+        const string path;
+        const bool isValid;
+        File(string path, bool isValid): isValid{isValid}, path{path} {}
 };
 
 
 class FileFactory {
+    bool fileExists(fs::file_status filestatus) {
+        return fs::exists(filestatus);
+    }
+
+    bool fileIsNotDirectory(fs::file_status filestatus) {
+        return !fs::is_directory(filestatus);
+    }
+
+    bool fileIsPermissive(fs::file_status filestatus) {
+        return (filestatus.permissions() & fs::perms::owner_read) != fs::perms::none;
+    }
+
+    bool fileIsReadable(string path) {
+        char byt;
+        bool isReadable = false;
+
+        fstream f;
+        f.open(path, ios::in | ios::binary);
+
+        if(f) {
+            try {
+                f.read(&byt, 1);
+                isReadable = true;
+            } catch(const std::exception& e) {
+                // TODO: do something here!
+            }
+        }
+
+        f.close();
+        return isReadable;
+    }
+
+    bool fileIsValid(string path) {
+        fs::file_status filestatus = fs::status(path);
+        return this->fileExists(filestatus)         && 
+               this->fileIsNotDirectory(filestatus) &&
+               this->fileIsPermissive(filestatus)   &&
+               this->fileIsReadable(path);
+    }
+
     public:
-        // TODO
-        static AbstractFile getFile(string path);
+        File* createFileType(string path) {
+            return new File(path, this->fileIsValid(path));
+        }
 };
 
 
@@ -49,33 +83,37 @@ class Hash {
     string calculatedHash;
     hashwrapper* hashWrapper;
     bool hashsumWasCalculated = false;
-    ValidFile* file;
+    File* file;
  
     void calculate(void) {
-        this->calculatedHash = this->hashWrapper->getHashFromFile(this->file->getPath());
+        this->calculatedHash = this
+                        ->hashWrapper
+                        ->getHashFromFile(this->file->path);
         this->hashsumWasCalculated = true;
     }
 
     public:
-        Hash(hashwrapper* hashWrapper, ValidFile* file) {
+        Hash(hashwrapper* hashWrapper, File* file) {
+            assert(file->isValid);
             this->hashWrapper = hashWrapper;
             this->file = file;
         }
-        
+
         string getStringHashSum() {
             if (this->hashsumWasCalculated) {
                 return this->calculatedHash;
             } this->calculate();
             return this->getStringHashSum();
         }
-        
+
         int getIntHashSum();
 };
 
 
 class HashFactory: public wrapperfactory {
     public:
-        Hash* hashFile(string hashtype, ValidFile* file) {
+        Hash* createFileHash(string hashtype, File* file) {
+            assert(file->isValid);
             return new Hash(this->create(hashtype), file);
         }
 };
@@ -88,10 +126,22 @@ class HashFactory: public wrapperfactory {
 
 
 int main() {
-    HashFactory hf;
-    ValidFile* f = new ValidFile("compile.sh");
-    Hash* h = hf.hashFile("MD5", f);
-    cout << h->getStringHashSum() << endl;
-    delete f, h;
+    HashFactory hashFactory;
+    FileFactory fileFactory;
+    string filepath = "./compile.sh";
+
+    File* file = fileFactory.createFileType(filepath);
+
+    if (file->isValid) {
+        Hash* hash = hashFactory.createFileHash("SHA1", file);
+        cout << "The SHA1SUM for '" << filepath << "' is: ";
+        cout << hash->getStringHashSum() << endl;
+        delete hash;
+    } else {
+        cout << "Could not calculate hash for '" << filepath << "'!" << endl;
+        cout << "File is invalid!" << endl;
+    }
+
+    delete file;
 	return 0;
 }
