343f8fddfe8167a761314c315282dfdbf6a5753e
anaximeno <anaximenobrito@gmail.com>
2022-02-24 19:34:12 -0100

    add argument option to the compile.sh script

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ OUTPUT_FILE=main
 
 echo "Compiling..."
 
-$COMPILER -std=$CPP_VERSION $FILE -I$INCLUDE_LIB -$LINK_LIB -o $OUTPUT_FILE
+$COMPILER -std=$CPP_VERSION $FILE -I$INCLUDE_LIB -$LINK_LIB -o $OUTPUT_FILE $@
 
 echo "Compilation finished!"
 echo "Now you can run the program using: ./main"
