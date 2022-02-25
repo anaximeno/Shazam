10242c7bb07a525297dcb9d68221b82b81829a35
anaximeno <anaximenobrito@gmail.com>
2022-02-25 21:05:46 -0100

    improved test script

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -4,23 +4,23 @@ COMPILER=g++
 TESTFILE="tests.cpp"
 CPP_VERSION=c++17
 ARGS=""
-OUTPUT_FILE=".test_result.tmp"
+OUTPUT_FILE=".test_result.shazam.tmp"
 
 # To analyse the error then, you'll only need to use
 # the command: cat test.last.log.txt
 TEST_LOG_FILE="test.last.log.txt"
 
-echo "[1] Compiling..."
-echo ""
+echo -n "[1] Compiling..."
 $COMPILER -std=$CPP_VERSION $TESTFILE -o $OUTPUT_FILE
-echo "[2] Running tests..."
+echo " Done!"
+echo ""
+echo -n "[2] Running tests..."
 ./$OUTPUT_FILE $ARGS > $TEST_LOG_FILE
+echo " Done!"
 echo ""
-echo "[3] Cleaning..."
+echo -n "[3] Cleaning..."
 rm $OUTPUT_FILE
+echo " Done!"
 echo ""
-echo "[4] Done!"
-echo ""
-echo ""
-echo -n "[5] Result: "
+echo -n "[4] Result: "
 cat $TEST_LOG_FILE
