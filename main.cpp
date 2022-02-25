445fa900cd9703cb5323e9cc632fd30831c8a1c9
anaximeno <anaximenobrito@gmail.com>
2022-02-25 15:16:27 -0100

    abstrated some classes and other definitions to shazam.hpp

--- main.cpp
+++ main.cpp
@@ -5,167 +5,13 @@
 #include <memory>
 #include <queue>
 
-#include <hashlibpp.h>
 #include "include/external/argparse.hpp"
-
-
-using std::cout, std::endl, std::string;
-using argparse::ArgumentParser;
-
-namespace fs = std::filesystem;
-
-
-
-/* Values:
-     NON_EXISTENT    -> file that wasn't found at the given path
-
-     IS_DIRECTORY    -> the path given points to a directory
-
-     NON_PERMISSIVE  -> The user don't have permissions to read the file
-
-     NOT_READABLE    -> file that cannot be read by some reason
-
-     IS_VALID        -> valid file that can be used withoud any problems
-*/
-enum EFileValidStatus {
-    IS_VALID,
-    NON_EXISTENT,
-    IS_DIRECTORY,
-    NOT_PERMISSIVE,
-    NOT_READABLE
-};
-
-
-class File {
-    const EFileValidStatus _status;
-    const string _path;
-    public:
-        File(string path, EFileValidStatus status):
-            _status{status}, _path{path} {
-            //
-        }
-
-        string path() {
-            return this->_path;
-        }
-
-        EFileValidStatus status() {
-            return this->_status;
-        }
-
-        bool isValid() {
-            return this->status() == EFileValidStatus::IS_VALID;
-        }
-};
-
-
-class FileFactory {
-    bool fileExists(fs::file_status filestatus) {
-        return fs::exists(filestatus);
-    }
-
-    bool fileIsNotDirectory(fs::file_status filestatus) {
-        return !fs::is_directory(filestatus);
-    }
-
-    bool fileIsPermissive(fs::file_status filestatus) {
-        return (filestatus.permissions() & fs::perms::owner_read) != fs::perms::none;
-    }
-
-    bool fileIsReadable(string path) {
-        char byt;
-        bool isReadable = false;
-
-        std::fstream f;
-        f.open(path, std::ios::in | std::ios::binary);
-
-        if(f) {
-            try {
-                f.read(&byt, 1);
-                isReadable = true;
-            } catch(const std::exception& e) {
-                // TODO: do something here!
-                // NOTE: IDEA: Maybe add this err to some log file.
-            }
-        }
-
-        f.close();
-        return isReadable;
-    }
-
-    protected:
-        EFileValidStatus fileValidStatus(string path) {
-            fs::file_status filestatus = fs::status(path);
-            if (!this->fileExists(filestatus))
-                return EFileValidStatus::NON_EXISTENT;
-            else if (!this->fileIsNotDirectory(filestatus))
-                return EFileValidStatus::IS_DIRECTORY;
-            else if (!this->fileIsPermissive(filestatus))
-                return EFileValidStatus::NOT_PERMISSIVE;
-            else if (!this->fileIsReadable(path))
-                return EFileValidStatus::NOT_READABLE;
-            else
-                return EFileValidStatus::IS_VALID;
-        }
-
-    public:
-        std::unique_ptr<File> createFileType(string path) {
-            return std::make_unique<File>(path, this->fileValidStatus(path));
-        }
-};
-
-
-class Hash {
-    string calculatedHash;
-    std::unique_ptr<hashwrapper> const hasher;
-    std::unique_ptr<File> const file;
-    bool hashsumWasCalculated = false;
-
-    protected:
-        void calculate(void) {
-            this->calculatedHash = this->hasher->getHashFromFile(this->file->path());
-            this->hashsumWasCalculated = true;
-        }
-
-    public:
-        Hash(std::unique_ptr<hashwrapper> hashWrapper, std::unique_ptr<File> file_ptr):
-            hasher{std::move(hashWrapper)}, file{std::move(file_ptr)} {
-            assert(file->isValid());
-        }
-
-        string getStringHashSum() {
-            if (this->hashsumWasCalculated) {
-                return this->calculatedHash;
-            } this->calculate();
-            return this->getStringHashSum();
-        }
-
-        int getIntHashSum();
-};
-
-
-class HashFactory: public wrapperfactory {
-    const std::array<string, 6> validHashTypes = {
-        "MD5", "SHA1", "SHA256", "SHA384", "SHA512"
-    };
-
-    public:
-        std::unique_ptr<Hash> createFileHash(string hashtype, std::unique_ptr<File> file) {
-            assert(file->isValid());
-            auto wrapper = std::unique_ptr<hashwrapper>(this->create(hashtype));
-            return std::make_unique<Hash>(std::move(wrapper), std::move(file));
-        }
-};
-
-
-class Checker {
-    // TODO: implement
-};
+#include "include/shazam.hpp"
 
 
 class App {
-    HashFactory hashFactory;
-    FileFactory fileFactory;
+    shazam::HashFactory hashFactory;
+    shazam::FileFactory fileFactory;
     std::unique_ptr<ArgumentParser> argparser;
     const string name;
 
@@ -198,21 +44,21 @@ class App {
                         .help("Use this to calculate the sha512 hash sum");
     }
 
-    string evaluateFileErr(EFileValidStatus errStatus) {
+    string evaluateFileErr(shazam::EFileValidStatus errStatus) {
         string err;
 
         switch (errStatus)
         {
-        case EFileValidStatus::NON_EXISTENT:
+        case shazam::EFileValidStatus::NON_EXISTENT:
             err = "File Not Found! ";
             break;
-        case EFileValidStatus::IS_DIRECTORY:
+        case shazam::EFileValidStatus::IS_DIRECTORY:
             err = "Directory given as file! ";
             break;
-        case EFileValidStatus::NOT_PERMISSIVE:
+        case shazam::EFileValidStatus::NOT_PERMISSIVE:
             err = "Permission was refused to read the file! ";
             break;
-        case EFileValidStatus::NOT_READABLE:
+        case shazam::EFileValidStatus::NOT_READABLE:
             err = "Can't read the file!";
             break;
         default:
