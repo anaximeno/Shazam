3b7ea0b20b9f371958b31e1afa3c6db6c9c0f791
anaximeno <anaximenobrito@gmail.com>
2022-02-25 17:49:33 -0100

    updated compiling process

--- compile.sh
+++ compile.sh
@@ -3,14 +3,12 @@
 COMPILER=g++
 CPP_VERSION=c++17
 FILE=main.cpp
-INCLUDE_LIB=/usr/include/hashlib++/
-LINK_LIB=lhl++
 OUTPUT_FILE=main
 
 
 echo "Compiling..."
 
-$COMPILER -std=$CPP_VERSION $FILE -I$INCLUDE_LIB -$LINK_LIB -o $OUTPUT_FILE $@
+$COMPILER -std=$CPP_VERSION $FILE -o $OUTPUT_FILE $@
 
 echo "Compilation finished!"
 echo "Now you can run the program using: ./main"
