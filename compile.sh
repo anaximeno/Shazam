d8e773e72442dfe8e49b0e2eeeae851d88bf587d
anaximeno <anaximenobrito@gmail.com>
2022-02-26 17:07:27 -0100

    Improved compiling script

--- compile.sh
+++ compile.sh
@@ -5,13 +5,18 @@ CPP_VERSION=c++17
 FILE=main.cpp
 OUTPUT_FILE=main
 
+# This pre out file can be used to verify if a new build
+# was created while preservating the last build if not.
+PRE_OUT_FILE=.$OUTPUT_FILE.pre.out
+
 # Now trying to compile the program:
 echo -n "Compiling..."
 
-$COMPILER -std=$CPP_VERSION $FILE -o $OUTPUT_FILE $@
+$COMPILER -std=$CPP_VERSION $FILE -o $PRE_OUT_FILE $@
 
-if [ -f $OUTPUT_FILE ]
+if [ -f $PRE_OUT_FILE ]
 then
+    mv $PRE_OUT_FILE $OUTPUT_FILE
     echo " Done!"
     echo "Run it using: ./main"
 else
